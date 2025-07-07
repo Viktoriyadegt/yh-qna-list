785bf9caa303748e0d27576d91a68c1a434b7d43
Viktoriyadegt <94902517+Viktoriyadegt@users.noreply.github.com>
2025-07-07 22:09:02 +0300

    fix texthtml component

--- src/shared/ui/TextHtml/TextHtml.tsx
+++ src/shared/ui/TextHtml/TextHtml.tsx
@@ -2,6 +2,7 @@ import DOMPurify from 'dompurify'
 import type { TextColor, TextVariant } from '@/shared/types/types.ts'
 import { Text } from '@/shared/ui'
 import s from './TextHtml.module.css'
+import clsx from 'clsx'
 
 type Props = {
   html: string
@@ -10,9 +11,9 @@ type Props = {
   className?: string
 }
 
-const TextHTML = ({ html, variant = 'body-16-med', color = 'black8' }: Props) => {
+const TextHTML = ({ html, variant = 'body-16-med', color = 'black8', className }: Props) => {
   return (
-    <div className={s.htmlText}>
+    <div className={clsx(s.htmlText, className)}>
       <Text variant={variant} color={color} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }} />
     </div>
   )
